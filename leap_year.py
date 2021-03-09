3582857aeba4ca68ba973486cf3748192718166c
iftun <72260688+iftuun@users.noreply.github.com>
2021-03-09 07:18:44 +0600

    Add files via upload

---
+++ leap_year.py
@@ -0,0 +1,10 @@
+yr=int(input("Which year do you want to check ? \n"))
+
+
+if (yr%4)==0:
+    if (yr%100)!=0 or (yr%400)==0:
+        print("This year "+ str(yr) + " is leap year")
+    else:
+        print("This year "+ str(yr) + " is not leap year")
+else:
+    print(f"This year {yr}, not leap year")
